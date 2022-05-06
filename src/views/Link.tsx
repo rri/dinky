b7ba492a97698b76c7c4fa6e2a748894b5b89d8f
Ramnath R Iyer <rri@silentyak.com>
2022-05-06 16:04:57 -0700

    Fix bug where link also triggered secondary action

--- src/views/Link.tsx
+++ src/views/Link.tsx
@@ -1,7 +1,16 @@
 import { NavLink } from "react-router-dom"
 
 export function renderLink(props: React.AnchorHTMLAttributes<HTMLAnchorElement>) {
-    const alink = () => <a href={props.href} title={props.children?.toString()}>{props.children}</a>
-    const nlink = () => <NavLink to={props.href ? props.href : ""}>{props.children}</NavLink>
+    const alink = () => <a
+        href={props.href}
+        title={props.children?.toString()}
+        onClick={e => e.stopPropagation()}
+    >{props.children}</a>
+
+    const nlink = () => <NavLink
+        to={props.href ? props.href : ""}
+        onClick={e => e.stopPropagation()}
+    >{props.children}</NavLink>
+
     return props.href && props.href.startsWith("/") ? nlink() : alink()
 }
