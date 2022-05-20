f6890e2b735739bd46128e925f27ee81eeb883fd
Ramnath R Iyer <riyer@optimix.dev>
2022-05-20 13:53:32 -0700

    Fix a storage bug
    In-memory state was being updated asynchronously, so the PUT to S3
    (based on in-memory state) had to be moved to a more appropriate point
    in the call flow.

--- src/models/Store.tsx
+++ src/models/Store.tsx
@@ -84,42 +84,44 @@ export class LocalStore {
         await client.send(get).then(res => {
             const { Body, ETag } = res
             readAndMergeData(Body as ReadableStream, ETag)
-        }).catch(err => {
-            const { $metadata: { httpStatusCode } } = err
-            checkHttpStatusCode(err, httpStatusCode)
-        })
-
-        const toExport: AppState = {
-            ...data,
-            settings: {
-                ...data.settings,
-                storage: {},
-            }
-        } as const
-
-        const put = new PutObjectCommand({
-            Bucket: data.settings.storage.s3Bucket,
-            Key: "data",
-            Body: JSON.stringify(toExport),
-            ContentType: "application/json",
-        })
-
-        await client.send(put).then(res => {
-            const { ETag } = res
-            this.setData(prev => {
-                const updated = {
-                    ...prev,
-                    settings: {
-                        ...prev.settings,
-                        storage: {
-                            ...prev.settings.storage,
-                            eTag: ETag,
+                .then(() => {
+                    const toExport: AppState = {
+                        ...data,
+                        settings: {
+                            ...data.settings,
+                            storage: {},
                         }
-                    }
-                }
-                updateLocalStorage("data", JSON.stringify(updated))
-                return updated
-            })
+                    } as const
+
+                    const put = new PutObjectCommand({
+                        Bucket: data.settings.storage.s3Bucket,
+                        Key: "data",
+                        Body: JSON.stringify(toExport),
+                        ContentType: "application/json",
+                    })
+
+                    client.send(put).then(res => {
+                        const { ETag } = res
+                        this.setData(prev => {
+                            const updated = {
+                                ...prev,
+                                settings: {
+                                    ...prev.settings,
+                                    storage: {
+                                        ...prev.settings.storage,
+                                        eTag: ETag,
+                                    }
+                                }
+                            }
+                            updateLocalStorage("data", JSON.stringify(updated))
+                            return updated
+                        })
+                    }).catch(err => {
+                        const { $metadata: { httpStatusCode } } = err
+                        checkHttpStatusCode(err, httpStatusCode)
+                    })
+
+                })
         }).catch(err => {
             const { $metadata: { httpStatusCode } } = err
             checkHttpStatusCode(err, httpStatusCode)
