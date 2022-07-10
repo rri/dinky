80319e1890598e36d9e4d506f26ed8797ab97eab
Ramnath R Iyer <rri@silentyak.com>
2022-07-09 23:19:51 -0700

    Updated reload icon
    Signed-off-by: Ramnath R Iyer <rri@silentyak.com>

--- src/views/Footer.tsx
+++ src/views/Footer.tsx
@@ -4,11 +4,9 @@ import styles from "../styles/Footer.module.css"
 export function Footer() {
     return (
         <Wrapper layout="row" className={styles.main}>
-            <div className={styles.reload} title="Reload the application" onClick={() => window.location.replace(window.location.href)}>⨀</div>
+            <div className={styles.copyright}>&#169; <a href="https://optimix.dev" title="Optimix Blog">Ramnath R Iyer</a></div>
             <div className={styles.dot}>&middot;</div>
-            <div className={styles.copyright}>&#169; Ramnath R Iyer</div>
-            <div className={styles.dot}>&middot;</div>
-            <div className={styles.blog}><a href="https://optimix.dev" title="Optimix Blog">https://optimix.dev</a></div>
+            <div className={styles.reload} title="Reload the application" onClick={() => window.location.replace(window.location.href)}>⥯</div>
         </Wrapper>
     )
 }
