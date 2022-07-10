6db1375a3b4bfea829a7cb40742b09e1bac5ce4d
Ramnath R Iyer <rri@silentyak.com>
2022-07-09 23:11:15 -0700

    Add reload button to footer

--- src/views/Footer.tsx
+++ src/views/Footer.tsx
@@ -4,6 +4,8 @@ import styles from "../styles/Footer.module.css"
 export function Footer() {
     return (
         <Wrapper layout="row" className={styles.main}>
+            <div className={styles.reload} title="Reload the application" onClick={() => window.location.replace(window.location.href)}>⨀</div>
+            <div className={styles.dot}>&middot;</div>
             <div className={styles.copyright}>&#169; Ramnath R Iyer</div>
             <div className={styles.dot}>&middot;</div>
             <div className={styles.blog}><a href="https://optimix.dev" title="Optimix Blog">https://optimix.dev</a></div>
