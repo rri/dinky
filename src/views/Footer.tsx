5c7bffced707ae5b9ad1787ec9299fbf45d0feba
Ramnath R Iyer <rri@silentyak.com>
2022-07-09 23:28:55 -0700

    Remove footer reload button (doesn't help)

--- src/views/Footer.tsx
+++ src/views/Footer.tsx
@@ -5,8 +5,6 @@ export function Footer() {
     return (
         <Wrapper layout="row" className={styles.main}>
             <div className={styles.copyright}>&#169; <a href="https://optimix.dev" title="Optimix Blog">Ramnath R Iyer</a></div>
-            <div className={styles.dot}>&middot;</div>
-            <div className={styles.reload} title="Reload the application" onClick={() => window.location.replace(window.location.href)}>⥯</div>
         </Wrapper>
     )
 }
