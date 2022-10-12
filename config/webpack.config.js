71ac11a977ee62ead7dd97a2695f852983d0205d
Ramnath R Iyer <riyer@optimix.dev>
2022-10-12 03:19:27 -0700

    Remove unnecessary TODO

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -351,8 +351,6 @@ module.exports = function (webpackEnv) {
           // match the requirements. When no loader matches it will fall
           // back to the "file" loader at the end of the loader list.
           oneOf: [
-            // TODO: Merge this config once `image/avif` is in the mime-db
-            // https://github.com/jshttp/mime-db
             {
               test: [/\.avif$/],
               type: 'asset',
